7479fe3406c4e535fa00b66d6b69bf5cbf59b22d
USER <USER@LAPTOP-O0KB79VJ>
2024-03-24 01:33:32 +0300

    Добавьте файлы проекта.

---
+++ kursovaya/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace ClassLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.create_button = new System.Windows.Forms.Button();
+            this.nametextbox = new System.Windows.Forms.TextBox();
+            this.populationtextbox = new System.Windows.Forms.TextBox();
+            this.descriptiontextbox = new System.Windows.Forms.TextBox();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.infoRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.find_button = new System.Windows.Forms.Button();
+            this.clean_button = new System.Windows.Forms.Button();
+            this.back_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listlabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statisticlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // create_button
+            // 
+            this.create_button.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.create_button.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.create_button.ForeColor = System.Drawing.Color.Black;
+            this.create_button.Location = new System.Drawing.Point(12, 402);
+            this.create_button.Name = "create_button";
+            this.create_button.Size = new System.Drawing.Size(195, 33);
+            this.create_button.TabIndex = 0;
+            this.create_button.Text = " Создать субъект";
+            this.create_button.UseVisualStyleBackColor = false;
+            this.create_button.Click += new System.EventHandler(this.create_button_Click);
+            // 
+            // nametextbox
+            // 
+            this.nametextbox.Location = new System.Drawing.Point(15, 114);
+            this.nametextbox.Name = "nametextbox";
+            this.nametextbox.Size = new System.Drawing.Size(195, 22);
+            this.nametextbox.TabIndex = 1;
+            // 
+            // populationtextbox
+            // 
+            this.populationtextbox.Location = new System.Drawing.Point(12, 167);
+            this.populationtextbox.Name = "populationtextbox";
+            this.populationtextbox.Size = new System.Drawing.Size(195, 22);
+            this.populationtextbox.TabIndex = 2;
+            // 
+            // descriptiontextbox
+            // 
+            this.descriptiontextbox.Location = new System.Drawing.Point(12, 227);
+            this.descriptiontextbox.Multiline = true;
+            this.descriptiontextbox.Name = "descriptiontextbox";
+            this.descriptiontextbox.Size = new System.Drawing.Size(195, 169);
+            this.descriptiontextbox.TabIndex = 3;
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.ItemHeight = 16;
+            this.listbox.Location = new System.Drawing.Point(247, 95);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(326, 260);
+            this.listbox.TabIndex = 4;
+            this.listbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listbox_MouseClick);
+            this.listbox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listbox_MouseDoubleClick);
+            // 
+            // infoRichTextBox
+            // 
+            this.infoRichTextBox.Location = new System.Drawing.Point(608, 95);
+            this.infoRichTextBox.Name = "infoRichTextBox";
+            this.infoRichTextBox.Size = new System.Drawing.Size(424, 301);
+            this.infoRichTextBox.TabIndex = 5;
+            this.infoRichTextBox.Text = "";
+            // 
+            // delete_button
+            // 
+            this.delete_button.BackColor = System.Drawing.Color.IndianRed;
+            this.delete_button.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delete_button.Location = new System.Drawing.Point(247, 401);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(326, 34);
+            this.delete_button.TabIndex = 6;
+            this.delete_button.Text = "Удалить субъект";
+            this.delete_button.UseVisualStyleBackColor = false;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // find_button
+            // 
+            this.find_button.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.find_button.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.find_button.Location = new System.Drawing.Point(15, 441);
+            this.find_button.Name = "find_button";
+            this.find_button.Size = new System.Drawing.Size(192, 29);
+            this.find_button.TabIndex = 7;
+            this.find_button.Text = "Найти по имени";
+            this.find_button.UseVisualStyleBackColor = false;
+            this.find_button.Click += new System.EventHandler(this.find_button_Click);
+            // 
+            // clean_button
+            // 
+            this.clean_button.BackColor = System.Drawing.Color.IndianRed;
+            this.clean_button.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clean_button.Location = new System.Drawing.Point(15, 476);
+            this.clean_button.Name = "clean_button";
+            this.clean_button.Size = new System.Drawing.Size(192, 46);
+            this.clean_button.TabIndex = 8;
+            this.clean_button.Text = "Очистить поля ввода";
+            this.clean_button.UseVisualStyleBackColor = false;
+            this.clean_button.Click += new System.EventHandler(this.clean_button_Click);
+            // 
+            // back_button
+            // 
+            this.back_button.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.back_button.Enabled = false;
+            this.back_button.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back_button.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.back_button.Location = new System.Drawing.Point(247, 363);
+            this.back_button.Name = "back_button";
+            this.back_button.Size = new System.Drawing.Size(326, 33);
+            this.back_button.TabIndex = 9;
+            this.back_button.Text = "Назад";
+            this.back_button.UseVisualStyleBackColor = false;
+            this.back_button.Click += new System.EventHandler(this.back_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Название субъекта:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Население:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Описание:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("durik", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(717, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Информация о субъекте:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Red October", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(300, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(480, 37);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Создание государсTва";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listlabel
+            // 
+            this.listlabel.AutoSize = true;
+            this.listlabel.BackColor = System.Drawing.Color.Transparent;
+            this.listlabel.Font = new System.Drawing.Font("durik", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listlabel.Location = new System.Drawing.Point(334, 72);
+            this.listlabel.Name = "listlabel";
+            this.listlabel.Size = new System.Drawing.Size(110, 19);
+            this.listlabel.TabIndex = 15;
+            this.listlabel.Text = "Все страны:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("durik", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(244, 453);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Статистика:";
+            // 
+            // statisticlabel
+            // 
+            this.statisticlabel.AutoSize = true;
+            this.statisticlabel.BackColor = System.Drawing.Color.Transparent;
+            this.statisticlabel.Font = new System.Drawing.Font("durik", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statisticlabel.Location = new System.Drawing.Point(244, 476);
+            this.statisticlabel.Name = "statisticlabel";
+            this.statisticlabel.Size = new System.Drawing.Size(0, 13);
+            this.statisticlabel.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.BackgroundImage = global::kursovaya.Properties.Resources.video_plug;
+            this.ClientSize = new System.Drawing.Size(1066, 603);
+            this.Controls.Add(this.statisticlabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listlabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.back_button);
+            this.Controls.Add(this.clean_button);
+            this.Controls.Add(this.find_button);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.infoRichTextBox);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.descriptiontextbox);
+            this.Controls.Add(this.populationtextbox);
+            this.Controls.Add(this.nametextbox);
+            this.Controls.Add(this.create_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button create_button;
+        private System.Windows.Forms.TextBox nametextbox;
+        private System.Windows.Forms.TextBox populationtextbox;
+        private System.Windows.Forms.TextBox descriptiontextbox;
+        private System.Windows.Forms.ListBox listbox;
+        private System.Windows.Forms.RichTextBox infoRichTextBox;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Button find_button;
+        private System.Windows.Forms.Button clean_button;
+        private System.Windows.Forms.Button back_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label listlabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label statisticlabel;
+    }
+}
+
---
+++ kursovaya/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary
+{
+    public partial class Form1 : Form
+    {
+        Counter counter;
+        SubjectService subjectService;
+        List<Subject> subjectIn = new List<Subject>() {new Host()};
+        Subject subjectCurrent = new Country();
+
+        public Form1()
+        {
+            InitializeComponent();
+            counter= new Counter();
+            subjectService = new SubjectService(counter);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statisticlabel.Text = ("Количество стран: 0\nКоличество регионов: 0\nКоличество районов: 0\nКоличество городов: 0");
+        }
+
+        private void create_button_Click(object sender, EventArgs e) // Создание субъекта классом SubjectService
+        {
+            Subject subject = subjectIn.Last();
+            if (typeof(Host) == subject.GetType())
+            {
+                subjectService.CreateCountry(nametextbox.Text, Convert.ToInt32(populationtextbox.Text), descriptiontextbox.Text); // Создание страны
+                listbox.DataSource = null;
+                listbox.DataSource = subjectService.countries;
+                listbox.DisplayMember = "Name"; // Вывод страны в listbox
+                
+            } else if (typeof (Country) == subject.GetType())
+            {
+                subjectService.CreateRegion((Country)subject, nametextbox.Text, Convert.ToInt32(populationtextbox.Text), descriptiontextbox.Text); // Создание региона
+                Country country = (Country)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = country.subjects;
+                listbox.DisplayMember = "Name";
+            } else if (typeof(Region) == subject.GetType())
+            {
+                subjectService.CreateDistrict((Region)subject, nametextbox.Text, Convert.ToInt32(populationtextbox.Text), descriptiontextbox.Text); // Создание района
+                Region region = (Region)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = region.subjects;
+                listbox.DisplayMember = "Name";
+
+            } else if (typeof(District) == subject.GetType())
+            {
+                subjectService.CreateCity((District)subject, nametextbox.Text, Convert.ToInt32(populationtextbox.Text), descriptiontextbox.Text); // СОздание города
+                District district = (District)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = district.subjects;
+                listbox.DisplayMember = "Name";
+            }
+            statisticlabel.Text = String.Format("Количество стран: {0}\nКоличество регионов: {1}\nКоличество районов: {2}\nКоличество городов: {3}", counter.CounterCountry, counter.CounterRegion, counter.CounterDistrict, counter.CounterCity);
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            Subject subject = subjectIn.Last();
+            if (typeof(Host) == subject.GetType())
+            {
+                subjectService.DeleteCountry((Country)listbox.SelectedItem); // Удаление страны из списка
+                listbox.DataSource = null;
+                listbox.DataSource = subjectService.countries;
+                listbox.DisplayMember = "Name";
+            }
+            else if (typeof(Country) == subject.GetType())
+            {
+                subjectService.DeleteRegion((Country)subject, (Region)listbox.SelectedItem); // Удаление региона
+                Country country = (Country)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = country.subjects;
+                listbox.DisplayMember = "Name";
+            }
+            else if (typeof(Region) == subject.GetType())
+            {
+                subjectService.DeleteDistrict((Region)subject, (District)listbox.SelectedItem); // Удаление района
+                Region region = (Region)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = region.subjects;
+                listbox.DisplayMember = "Name";
+
+            }
+            else if (typeof(District) == subject.GetType())
+            {
+                subjectService.DeleteCity((District)subject, (City)listbox.SelectedItem); // Удаление города
+                District district = (District)subject;
+                listbox.DataSource = null;
+                listbox.DataSource = district.subjects;
+                listbox.DisplayMember = "Name";
+            }
+            statisticlabel.Text = String.Format("Количество стран: {0}\nКоличество регионов: {1}\nКоличество районов: {2}\nКоличество городов: {3}", counter.CounterCountry, counter.CounterRegion, counter.CounterDistrict, counter.CounterCity);
+        }
+
+        private void find_button_Click(object sender, EventArgs e) // Поиск субъекта
+        {
+            subjectService.Note += DisplayMessage;
+            subjectCurrent = subjectService.FindSubject(nametextbox.Text);
+            if (subjectCurrent != null)
+            { 
+                infoRichTextBox.Clear();
+                infoRichTextBox.Text = subjectCurrent.ToString();             
+            }
+        }
+        private static void DisplayMessage(Object sender, SubjectEventArgs e) // Сообщение на форме
+        {
+            if (e.message != "По вашему запросу ничего не найдено ;(")
+            {
+                MessageBox.Show(String.Format(e.message));
+            }
+            else
+            {
+               MessageBox.Show(String.Format(e.message));
+            }
+        }
+
+        private void clean_button_Click(object sender, EventArgs e) // Очищение полей ввода
+        {
+            nametextbox.Clear();
+            populationtextbox.Clear();
+            descriptiontextbox.Clear();
+        }
+
+        private void listbox_MouseClick(object sender, MouseEventArgs e) // Вывод информации о выбранном субъекте по клику
+        {
+            try
+            {
+                infoRichTextBox.Clear();
+                infoRichTextBox.Text = listbox.SelectedItem.ToString();
+            } catch (NullReferenceException)
+            {
+                return;
+            }
+            
+            
+        }
+
+        private void listbox_MouseDoubleClick(object sender, MouseEventArgs e) // Переход к вложенному списку в субъекте по двойному клику
+        {
+            try {
+                subjectIn.Add((Subject)listbox.SelectedItem);
+                Subject subject = subjectIn.Last();
+
+                if (typeof(Country) == subject.GetType())
+                {
+                    Country country = (Country)subject;
+                    listbox.DataSource = country.subjects;
+                    listlabel.Text = "Все регионы:";
+                    back_button.Enabled = true;
+                }
+                else if (typeof(Region) == subject.GetType())
+                {
+                    Region region = (Region)subject;
+                    listbox.DataSource = region.subjects;
+                    listlabel.Text = "Все районы:";
+                }
+                else if (typeof(District) == subject.GetType())
+                {
+                    District district = (District)subject;
+                    listbox.DataSource = district.subjects;
+                    listlabel.Text = "Все города:";
+                }
+            } catch (NullReferenceException) { return; }
+           
+        }
+
+        private void back_button_Click(object sender, EventArgs e) // Возвращение листбокса к предыдущему списку
+        {
+            if (subjectIn.Count == 1) {
+                return;
+            }
+            subjectIn.RemoveAt(subjectIn.Count - 1);
+            Subject subject = subjectIn.Last();
+           
+            if (typeof(Country) == subject.GetType())
+            {
+                Country country = (Country)subject;
+                listbox.DataSource = country.subjects;
+                listbox.DisplayMember = "Name";
+                listlabel.Text = "Все регионы:";
+            }
+            else if (typeof(Region) == subject.GetType())
+            {
+                Region region= (Region)subject;
+                listbox.DataSource = region.subjects;
+                listbox.DisplayMember = "Name";
+                listlabel.Text = "Все районы:";
+            }
+            else if (typeof(District) == subject.GetType())
+            {
+                District district = (District)subject;
+                listbox.DataSource = district.subjects;
+                listbox.DisplayMember = "Name";
+                listlabel.Text = "Все города:";
+            } else if (typeof(Host) == subject.GetType())
+            {
+                listbox.DataSource = subjectService.countries;
+                listbox.DisplayMember = "Name";
+                listlabel.Text = "Все страны:";
+                back_button.Enabled = false;
+            } else if (typeof(City) == subject.GetType())
+            {
+                return;
+            }
+        }
+    }
+}
